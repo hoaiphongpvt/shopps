67b23dacadf27c4e4ab2e1ba2137745980f156a8
hoaiphongpvt <hoaiphongpvt@gmail.com>
2023-08-09 17:56:58 +0700

    Update

--- mvc/views/home.php
+++ mvc/views/home.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD Student</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../../public/images/logo.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="../../public/css/style.css">
 </head>
 <body>
     <div class="container pt-4">
